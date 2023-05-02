d11cdaf39c0cfecf7b3c3de2040a6bae03337fa7
durgesh <durgeshchaudhary020401@gmail.com>
2023-05-02 12:12:33 +0530

    data filter according user

--- client/src/pages/Tours.jsx
+++ client/src/pages/Tours.jsx
@@ -1,47 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/tour.css";
 import img from "../assets/images/gallery-01.jpg";
 
 const Tours = () => {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const response = await fetch(`http://localhost:5000/data/UserEnteredData`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "token": localStorage.getItem("token")
+      },
+    });
+    const json = await response.json();
+    setData(json);
+    // console.log(json,"sklamdl")
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+
   return (
-    <section class="expert" id="experts">
-      <h1 class="heading">
-        place <span>visited</span>?
-      </h1>
-      <div class="container">
-        <div class="d-flex flex-wrap justify-content-center">
-          <div class="box p-4 m-2">
-            <img src={img} />
-            <div class="icons">
-              <div className="first">Placename</div>
-              <div classNaame="second">
-                <table>
-                  <tr>
-                    <th>coity</th>
-                    <th>state</th>
-                  </tr>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div class="box p-4 m-2">
-            <img src={img} />
-            <h3>Priti</h3>
-            <span>virus expert</span>
-          </div>
-          <div class="box p-4 m-2">
-            <img src={img} />
-            <h3>Priti</h3>
-            <span>virus expert</span>
-          </div>
-          <div class="box p-4 m-2">
-            <img src={img} />
-            <h3>Harsh</h3>
-            <span>virus expert</span>
-          </div>
+    <>
+
+      <div className="container" >
+        <div className="cardstext" >
+          {
+            data?.map((items, index) => {
+              return (
+                <>
+                  <div className="startcard" >
+                    <div>
+                      <img src={items.photo} ></img>
+                    </div>
+                    <div>
+                      <span>{items.city}</span>
+                    </div>
+                    <div>
+                      <span>{items.placeName}</span>
+                    </div>
+
+                    <div>
+                      <span>{items.state}</span>
+                    </div>
+                  </div>
+
+                </>
+              )
+            })
+          }
+
         </div>
       </div>
-    </section>
+
+    </>
   );
 };
 
